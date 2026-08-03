08693402de1d754264afc3730116b492ecffbf6a
mateimitnei <matei.mitnei@stud.acs.upb.ro>
2026-08-03 15:46:39 +0300

    Implemented TicketService with Observable

---
+++ src/app/services/ticket.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import { ITicket } from '../ticket.model';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TicketService {
+  private tickets : ITicket[] = [
+    {
+      id: 1,
+      ticketKey: 'TK-114',
+      title: 'Sample Ticket',
+      description: 'This is the first mock ticket ever created for this frontend project.',
+      createdAt: new Date(),
+      statusId: 1,
+      priorityId: 2
+    },
+    {
+      id: 2,
+      ticketKey: 'TK-115',
+      title: 'Ssample Ticket',
+      description: 'This is the second mock ticket ever created for this frontend project.',
+      createdAt: new Date(),
+      statusId: 2,
+      priorityId: 1
+    },
+    {
+      id: 3,
+      ticketKey: 'TK-116',
+      title: 'Sample Ticcket',
+      description: 'This is the third mock ticket ever created for this frontend project.',
+      createdAt: new Date(),
+      statusId: 3,
+      priorityId: 3
+    },
+    {
+      id: 4,
+      ticketKey: 'TK-117',
+      title: 'Sample Tickett',
+      description: 'This is the fourth mock ticket ever created for this frontend project.',
+      createdAt: new Date(),
+      statusId: 1,
+      priorityId: 2
+    },
+    {
+      id: 5,
+      ticketKey: 'TK-118',
+      title: 'Sammple Ticket',
+      description: 'This is the fifth mock ticket ever created for this frontend project.',
+      createdAt: new Date(),
+      statusId: 2,
+      priorityId: 1
+    },
+    {
+      id: 6,
+      ticketKey: 'TK-119',
+      title: 'Ssammple Ticckett',
+      description: 'This is the sixth mock ticket ever created for this frontend project.',
+      createdAt: new Date(),
+      statusId: 3,
+      priorityId: 3
+    }
+  ]
+
+  getTickets(): Observable<ITicket[]> {
+    return of(this.tickets);
+  }
+
+  getLength(): number {
+    return this.tickets.length;
+  }
+
+  searchTickets(searchText: string): Observable<ITicket[]> {
+    const filteredTickets = this.tickets.filter(ticket => 
+      ticket.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    return of(filteredTickets);
+  }
+
+  addTicket(ticket: ITicket) {
+    this.tickets.push(ticket);
+  }
+
+  deleteTicketById(id: number) {
+    this.tickets = this.tickets.filter(t => t.id !== id);
+  }
+}
--- src/app/ticket-list/ticket-list.component.ts
+++ src/app/ticket-list/ticket-list.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { TicketCardComponent } from '../ticket-card/ticket-card.component';
 import { FormsModule } from '@angular/forms';
 import { ITicket } from '../ticket.model';
+import { TicketService } from '../services/ticket.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-ticket-list',
-  imports: [TicketCardComponent, FormsModule],
+  imports: [TicketCardComponent, FormsModule, AsyncPipe],
   templateUrl: './ticket-list.component.html',
   styleUrl: './ticket-list.component.css',
 })
@@ -14,71 +17,20 @@ export class TicketListComponent {
   searchText: string = '';
   newTicketTitle: string = '';
   newTicketPriorityId: number = 0;
-  tickets : ITicket[] = [
-    {
-      id: 1,
-      ticketKey: 'TK-114',
-      title: 'Sample Ticket',
-      description: 'This is the first mock ticket ever created for this frontend project.',
-      createdAt: new Date(),
-      statusId: 1,
-      priorityId: 2
-    },
-    {
-      id: 2,
-      ticketKey: 'TK-115',
-      title: 'Ssample Ticket',
-      description: 'This is the second mock ticket ever created for this frontend project.',
-      createdAt: new Date(),
-      statusId: 2,
-      priorityId: 1
-    },
-    {
-      id: 3,
-      ticketKey: 'TK-116',
-      title: 'Sample Ticcket',
-      description: 'This is the third mock ticket ever created for this frontend project.',
-      createdAt: new Date(),
-      statusId: 3,
-      priorityId: 3
-    },
-    {
-      id: 4,
-      ticketKey: 'TK-117',
-      title: 'Sample Tickett',
-      description: 'This is the fourth mock ticket ever created for this frontend project.',
-      createdAt: new Date(),
-      statusId: 1,
-      priorityId: 2
-    },
-    {
-      id: 5,
-      ticketKey: 'TK-118',
-      title: 'Sammple Ticket',
-      description: 'This is the fifth mock ticket ever created for this frontend project.',
-      createdAt: new Date(),
-      statusId: 2,
-      priorityId: 1
-    },
-    {
-      id: 6,
-      ticketKey: 'TK-119',
-      title: 'Ssammple Ticckett',
-      description: 'This is the sixth mock ticket ever created for this frontend project.',
-      createdAt: new Date(),
-      statusId: 3,
-      priorityId: 3
-    }
-  ]
+  ticketsObs: Observable<ITicket[]> | undefined;
+  
+  constructor(private ticketService: TicketService) {}
 
-  get filteredTickets() {
+  ngOnInit(): void {
+    this.ticketsObs = this.ticketService.getTickets();
+  }
+
+  onSearch(): void {
     if (!this.searchText) {
-      return this.tickets;
+      this.ticketsObs = this.ticketService.getTickets();
+    } else {
+      this.ticketsObs = this.ticketService.searchTickets(this.searchText);
     }
-
-    return this.tickets.filter(
-      ticket => ticket.title.toLowerCase().includes(this.searchText.toLowerCase())
-    );
   }
 
   addTicket() {
@@ -86,8 +38,8 @@ export class TicketListComponent {
       return;
     }
     const newTicket = {
-      id: this.tickets.length + 1,
-      ticketKey: `TK-${114 + this.tickets.length}`,
+      id: this.ticketService.getLength() + 1,
+      ticketKey: `TK-${114 + this.ticketService.getLength()}`,
       title: this.newTicketTitle,
       description: `User created a ticket after clicking the "Add Ticket" button.`,
       createdAt: new Date(),
@@ -95,12 +47,14 @@ export class TicketListComponent {
       priorityId: this.newTicketPriorityId
     };
 
-    this.tickets = [newTicket, ...this.tickets];
+    this.ticketService.addTicket(newTicket);
+    this.onSearch(); // update ticket list (keeping the search filter)
     this.newTicketTitle = '';
     this.newTicketPriorityId = 0;
   }
 
   deleteTicketById(ticketId: number) {
-    this.tickets = this.tickets.filter(ticket => ticket.id !== ticketId);
+    this.ticketService.deleteTicketById(ticketId);
+    this.onSearch(); // update ticket list
   }
 }
